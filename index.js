840acd4bdd14aeba0b3e4d2ebcb357214d0703c4
premkumar2004 <131762000+premkumar2004@users.noreply.github.com>
2023-10-14 11:19:46 +0530

    Update index.js

--- index.js
+++ index.js
@@ -467,7 +467,7 @@ app.get('/register', async (req, res) => {
   try {
     const textToDisplay = "";
    await res.render('reg', {textToDisplay});
-    res.redirect('/);
+    res.redirect('/');
     
     
   } catch (err) {
